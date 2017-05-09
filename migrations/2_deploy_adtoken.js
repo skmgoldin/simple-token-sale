9afd97518f5c3b1aff49e289e93ae41c00eb816c
Mike Goldin <mike.goldin@consensys.net>
2017-05-09 14:07:48 -0700

    Update adtoken deployment

--- migrations/2_deploy_adtoken.js
+++ migrations/2_deploy_adtoken.js
@@ -1,2 +1,12 @@
+const Token = artifacts.require(`./HumanStandardToken.sol`);
+const fs = require(`fs`);
+
 module.exports = function(deployer) {
+    const tokenConf = JSON.parse(fs.readFileSync(`./conf/token.json`));
+    deployer.deploy(Token,
+      tokenConf.initialAmount,
+      tokenConf.tokenName,
+      tokenConf.decimalUnits,
+      tokenConf.tokenSymbol
+    );
 };
