09b34592ffb7459d75f6f916e69db646acfe37c4
Mike Goldin <mike.goldin@consensys.net>
2017-05-09 14:26:00 -0700

    Add dev logic and safeguards

--- migrations/2_deploy_adtoken.js
+++ migrations/2_deploy_adtoken.js
@@ -1,7 +1,7 @@
 const Token = artifacts.require(`./HumanStandardToken.sol`);
 const fs = require(`fs`);
 
-module.exports = function(deployer) {
+module.exports = function(deployer, network, accounts) {
     const tokenConf = JSON.parse(fs.readFileSync(`./conf/token.json`));
     deployer.deploy(Token,
       tokenConf.initialAmount,
--- migrations/3_deploy_sale.js
+++ migrations/3_deploy_sale.js
@@ -2,8 +2,19 @@ const Sale = artifacts.require(`./Sale.sol`);
 const Token = artifacts.require(`./HumanStandardToken.sol`);
 const fs = require(`fs`);
 
-module.exports = function(deployer) {
+module.exports = function(deployer, network, accounts) {
     const saleConf = JSON.parse(fs.readFileSync(`./conf/sale.json`));
+    if(network === `development`) {
+      saleConf.owner = accounts[0];
+      saleConf.wallet = accounts[0];
+      saleConf.prod = false;
+      fs.writeFileSync(`./conf/sale.json`, JSON.stringify(saleConf, null, `  `));
+    }
+    if(network === `mainnet`) {
+      if(saleConf.prof === false) {
+        throw new Error(`prod flag in sale.json is false. Review it manually.`);
+      }
+    }
     deployer.deploy(Sale,
       saleConf.owner,
       saleConf.wallet,
