75eaa77e7be69d7c05b2fe3ae7051f144c13396a
Mike Goldin <mike.goldin@consensys.net>
2017-06-06 12:44:36 -0400

    Update deploy logic for atomic instantiation

--- migrations/2_deploy_adtoken.js
+++ migrations/2_deploy_adtoken.js
@@ -1,9 +1,13 @@
 const Sale = artifacts.require(`./Sale.sol`);
 const Token = artifacts.require(`./HumanStandardToken.sol`);
 const fs = require(`fs`);
+const BN = require(`bn.js`);
 
 module.exports = function(deployer, network, accounts) {
     const saleConf = JSON.parse(fs.readFileSync(`./conf/sale.json`));
+    const tokenConf = JSON.parse(fs.readFileSync(`./conf/token.json`));
+    const distros = JSON.parse(fs.readFileSync(`./conf/distros.json`));
+
     if(network === `development`) {
       saleConf.owner = accounts[0];
       saleConf.wallet = `0x000000000000000000000000000000000000dead`;
@@ -17,12 +21,24 @@ module.exports = function(deployer, network, accounts) {
       }
     }
 
-    deployer.deploy(Sale,
+    const founders = [];
+    const foundersTokens = [];
+    for (recipient in distros) {
+      founders.push(distros[recipient].address); 
+      foundersTokens.push(new BN(distros[recipient].amount, 10)); 
+    }
+
+    return deployer.deploy(Sale,
       saleConf.owner,
       saleConf.wallet,
-      Token.address,
+      tokenConf.initialAmount,
+      tokenConf.tokenName,
+      tokenConf.decimalUnits,
+      tokenConf.tokenSymbol,
       saleConf.price,
       saleConf.startBlock,
-      saleConf.freezeBlock
-    );
+      saleConf.freezeBlock,
+      founders,
+      foundersTokens
+    )
 };
