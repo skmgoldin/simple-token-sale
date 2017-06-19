8b59178963a18bc67f661f5b24242e60dbff8747
Mike Goldin <mike.goldin@consensys.net>
2017-06-18 20:06:36 -0400

    Add comments

--- contracts/Sale.sol
+++ contracts/Sale.sol
@@ -128,8 +128,8 @@ contract Sale {
         require(preSaleTokensDisbursed);
         require(!foundersTokensDisbursed);
 
-        // Total number of tokens to be disbursed for a given tranch. Used when
-        // tokens are transferred to disbursement contracts.
+        /* Total number of tokens to be disbursed for a given tranch. Used when
+           tokens are transferred to disbursement contracts. */
         uint tokensPerTranch = 0;
         // Alias of founderTimelocks.length for legibility
         uint tranches = _founderTimelocks.length;
@@ -142,17 +142,17 @@ contract Sale {
             tokensPerTranch = tokensPerTranch + foundersTokensPerTranch[i];
         }
 
-        // Deploy disbursement and filter contract pairs, initialize both and store
-        // filter addresses in filters array. Finally, transfer tokensPerTranch to
-        // disbursement contracts.
+        /* Deploy disbursement and filter contract pairs, initialize both and store
+           filter addresses in filters array. Finally, transfer tokensPerTranch to
+           disbursement contracts. */
         for(uint j = 0; j < tranches; j++) {
             Filter filter = new Filter(_founders, foundersTokensPerTranch);
             filters.push(filter);
             Disbursement vault = new Disbursement(filter, 1, _founderTimelocks[j]);
             // Give the disbursement contract the address of the token it disburses.
             vault.setup(token);             
-            // Give the filter contract the address of the disbursement contract
-            // it access controls
+            /* Give the filter contract the address of the disbursement contract
+               it access controls */
             filter.setup(vault);             
             // Transfer to the vault the tokens it is to disburse
             assert(token.transfer(vault, tokensPerTranch));
@@ -165,25 +165,32 @@ contract Sale {
 
     /// @dev purchaseToken(): function that exchanges ETH for ADT (main sale function)
     /// @notice You're about to purchase the equivalent of `msg.value` Wei in ADT tokens
-    // TODO: Add commments explaining edge cases, etc.
     function purchaseTokens()
         saleStarted
         payable
         setupComplete
         notInEmergency
     {
+        /* Calculate whether any of the msg.value needs to be returned to
+           the sender. The tokenPurchase is the actual number of tokens which
+           will be purchased once any excessAmount included in the msg.value
+           is removed from the purchaseAmount. */
         uint excessAmount = msg.value % price;
         uint purchaseAmount = msg.value - excessAmount;
         uint tokenPurchase = purchaseAmount / price;
 
+        // Cannot purchase more tokens than this contract has available to sell
         require(tokenPurchase <= token.balanceOf(this));
 
+        // Return any excess msg.value
         if (excessAmount > 0) {
             msg.sender.transfer(excessAmount);
         }
 
+        // Forward received ether minus any excessAmount to the wallet
         wallet.transfer(purchaseAmount);
 
+        // Transfer the sum of tokens tokenPurchase to the msg.sender
         assert(token.transfer(msg.sender, tokenPurchase));
 
         PurchasedTokens(msg.sender, tokenPurchase);
